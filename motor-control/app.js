17c27edaae1ad0934d9d4416c735ab4eb15acb40
Ryan Jacobs <ryan.mjacobs@gmail.com>
2017-02-27 13:10:21 -0800

    use neg for int values

--- motor-control/app.js
+++ motor-control/app.js
@@ -4,10 +4,14 @@ let WebSocket = require("ws");
 let ws = new WebSocket("ws://localhost:9987");
 
 let STATE = {
-    motors: [false, false, false],
-    direction: false,
-    engaged: false
+    motors: [0, 0, 0],
+    direction: 0,
+    engaged: 0
 };
+
+function neg(arg) {
+    return arg ? 0 : 1;
+}
 
 ws.onmessage = function(msg) {
     let data = JSON.parse(msg.data);
@@ -17,27 +21,27 @@ ws.onmessage = function(msg) {
         case "client-click":
             switch (data.which) {
                 case "motor-btn-1":
-                    STATE.motors[0] = !STATE.motors[0];
+                    STATE.motors[0] = neg(STATE.motors[0]);
                     break;
 
                 case "motor-btn-2":
-                    STATE.motors[1] = !STATE.motors[1];
+                    STATE.motors[1] = neg(STATE.motors[1]);
                     break;
 
                 case "motor-btn-3":
-                    STATE.motors[2] = !STATE.motors[2];
+                    STATE.motors[2] = neg(STATE.motors[2]);
                     break;
 
                 case "arrow-btn-left":
-                    STATE.direction = false;
+                    STATE.direction = 0;
                     break;
 
                 case "arrow-btn-right":
-                    STATE.direction = true;
+                    STATE.direction = 1;
                     break;
 
                 case "engage-btn":
-                    STATE.engaged = !STATE.engaged;
+                    STATE.engaged = neg(STATE.engaged);
                     break;
             }
 
