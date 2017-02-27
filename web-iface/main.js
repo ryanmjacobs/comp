fd37fa3da94ba333d3112214fa45bb04f66b89d6
Ryan Jacobs <ryan.mjacobs@gmail.com>
2017-02-27 05:02:13 -0800

    clean and functional

--- web-iface/main.js
+++ web-iface/main.js
@@ -14,15 +14,24 @@ ws.onopen = function() {
 
 function draw(state) {
     state = {
-        motors: [false, false, true],
+        motors: [true, false, true],
         direction: true,
-        engage: true
+        engage: false
     };
 
-    for (let i = 0; i < 3; i++) {
-        let el = document.getElementById("motor-btn-" + (i+1));
-        el.className = state.motors[i] ? "btn on" : "btn off";
+    function set_btn(which, state) {
+        let el = document.getElementById(which);
+        el.classList.remove(["on", "off"]);
+        el.classList.add(state ? "on" : "off");
     }
+
+    for (let i = 0; i < 3; i++) {
+        set_btn("motor-btn-" + (i+1), state.motors[i]);
+    }
+
+    set_btn("arrow-btn-left", !state.direction);
+    set_btn("arrow-btn-right", state.direction);
+    set_btn("engage-btn", state.engage);
 }
 
 draw();
